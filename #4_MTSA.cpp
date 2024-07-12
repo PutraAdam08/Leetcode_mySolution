664b08cedf999ed20a25ecf094c5231387cc48a8
Putra Adam <putraadam322@gmail.com>
2024-07-12 14:38:55 +0700

    Finish with 28ms runtime

--- #4_MTSA.cpp
+++ #4_MTSA.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution { // Using Two Pointer
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         /*if((nums1.size() + nums2.size())%2 != 0){
@@ -9,32 +9,30 @@ public:
             else return nums2.front();
         }*/
         int n = nums2.size() + nums1.size();
-        int i = 0, j = 0 ,temp = -1;
+        int i = 0, j = 0 , n1 = 0, n2 = 0;
 
-        if(n/2 == 0) temp == 0;
         //vector<int> temp;
-        while(i + j < nums2.size() + nums1.size()){
-            if(temp == n/2){
-                break;
+        for(int temp = 0; temp <= n/2; temp++){
+            n2 = n1;
+            if(i != nums1.size() && j != nums2.size()){
+                if(nums1[i] < nums2[j]){
+                    n1 = nums1[i++];
+                }
+                else
+                    n1 = nums2[j++];
             }
-            if(nums1[i] <= nums2[j] && i < nums1.size()){
-                //temp.push_back(nums1[i]);
-                i++;
-                temp++;
-            }
-            else{
-                //temp.push_back(nums2[j]);
-                if(j < nums2.size()-1)
-                    j++;
-                temp++;
-            }
-            temp = i+j;
+            else if(i < nums1.size())
+                n1 = nums1[i++];
+            else
+                n1 = nums2[j++];
         }
 
-        if(n%2 == 0)
-            return (double)(nums1[i-1]+nums2[j])/2;
+        if(n%2 == 0){
+            double ans = (double)(n1+n2);
+            return ans / 2.0;
+        }
         else
-            return nums2[j];
+            return n1;/**/
 
         return 0;
     }
@@ -42,8 +40,8 @@ public:
 
 int main(){
     Solution s;
-    vector<int> nums1 = {};
-    vector<int> nums2 = {1};
+    vector<int> nums1 = {2, 3};
+    vector<int> nums2 = {1, 4};
     double o = s.findMedianSortedArrays(nums1, nums2);
 
     cout << o;
